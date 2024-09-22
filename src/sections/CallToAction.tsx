07c6845ae8430d423465c23edff148af85bb627a
Devang1224 <97470914+Devang1224@users.noreply.github.com>
2024-09-23 00:49:42 +0530

    inital commit

---
+++ src/sections/CallToAction.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import ArrowRight from "@/assets/arrow-right.svg"
+import StarImage from "@/assets/star.png"
+import SpringImage from "@/assets/spring.png"
+import Image from "next/image";
+import {motion, useScroll, useTransform} from "framer-motion"
+import { useRef } from "react";
+
+
+
+export const CallToAction = () => {
+
+
+  const sectionRef = useRef(null);
+
+  const {scrollYProgress} = useScroll({
+     target:sectionRef,
+     offset:['start end','end start']
+  })
+
+  const translateY = useTransform(scrollYProgress,[0,1],[150,-150]);
+
+  return (
+    <section ref={sectionRef} className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+      <div className="container">
+         <div className="section-heading relative">
+           <h2 className="section-title">Sign up for free today</h2>
+           <p className="section-desc mt-5">
+             Celebrate the joy of accomplishment with an app designed to track your progress and motivte your efforts.
+            </p>
+           <motion.img src={StarImage.src} alt="Star Image" width={360} className="hidden md:block absolute -left-[350px] -top-[137px]"
+            style={{
+              translateY:translateY
+            }}
+           />
+           <motion.img src={SpringImage.src} alt="Spring Image" width={360} className="hidden md:block absolute -right-[331px] -top-[19px]"
+            style={{
+              translateY:translateY
+            }}
+           />
+         </div>
+         <div className="flex justify-center gap-2 mt-10">
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn btn-text">
+           <span>
+             Learn more
+           </span> 
+          <ArrowRight className="h-5 w-5"/>
+          </button>
+         </div>
+      </div>
+    </section>
+  )
+};
